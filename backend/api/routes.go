9299062f69138bb480bf673b2be7028cef993cc9
Masahiko AMANO <hiko-19@ya.ru>
2025-01-06 03:18:06 +0300

    init(api): add routes.go file and update go.mod and go.sum

---
+++ backend/api/routes.go
@@ -0,0 +1,25 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+// @title SkazaNull API
+// @description RESTful API для пацанского цитатника SkazaNull
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host skazanull.hakoniwa.ru
+// @BasePath /api
+func RegisterRoutes(r *gin.Engine) {
+	api := r.Group("/api")
+	{
+		api.POST("/auth", userAuth)
+	}
+}
